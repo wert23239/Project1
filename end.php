148f2fe461554ee44102bf9b809c69cc87ef5ed7
winskyst-msu <winskyst@msu.edu>
2016-02-17 19:07:16 -0500

    css done for now

--- end.php
+++ end.php
@@ -12,81 +12,90 @@ require 'format.inc.php';
 </head>
 <body>
 
-    <?php echo present_header(); ?>
+    <div class = "content">
+        <div class = "header"><?php echo present_header(); ?></div>
 
-    <form ...>
-        <div class="game">
-            <div class="row">
-                <div class="cell"><img src="images/valve-open.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/ninety-sw.png" alt="cave" width="50" height="50" />
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell"><img src="images/gauge-top-190.png" alt="cave" width="50" height="50" /></div>
-            </div>
-            <div class="row">
-                <div class="cell">
-                </div><div class="cell"><img src="images/ninety-ne.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/gauge-190.png" alt="cave" width="50" height="50" /></div>
-            </div>
-            <div class="row">
-                <div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell"></div>
-            </div>
-            <div class="row">
-                <div class="cell">
-                </div><div class="cell">
-                </div><div class="cell"><img src="images/cap-s.png" alt="cave" width="50" height="50" />
-                </div><div class="cell">
-                </div><div class="cell"><img src="images/leak-s.png" alt="cave" width="50" height="50" />
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell"><img src="images/gauge-top-190.png" alt="cave" width="50" height="50" />
+        <form>
+            <div class="game">
+                <div class="row">
+                    <div class="cell"><img src="images/valve-open.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/ninety-sw.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/gauge-top-190.png" alt="cave" width="50" height="50" /></div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell"><img src="images/ninety-ne.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/gauge-190.png" alt="cave" width="50" height="50" /></div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"></div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/cap-s.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/leak-s.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/gauge-top-0.png" alt="cave" width="50" height="50" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/tee-nes.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/tee-wne.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/leak-w.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/gauge-0.png" alt="cave" width="50" height="50" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="cell"><img src="images/valve-open.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/ninety-wn.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="cell">
-                </div><div class="cell">
-                </div><div class="cell"><img src="images/tee-nes.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/tee-wne.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/leak-w.png" alt="cave" width="50" height="50" />
-                </div><div class="cell">
-                </div><div class="cell"><img src="images/gauge-190.png" alt="cave" width="50" height="50" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="cell"><img src="images/valve-open.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-                </div><div class="cell"><img src="images/ninety-wn.png" alt="cave" width="50" height="50" />
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div><div class="cell">
-                </div>
-            </div>
+        </form>
+
+
+        <div class = "winner">
+            <p class = "win">Player 1 Wins!</p>
+            <p class = "lose">Player 2 Loses!</p>
         </div>
-    </form>
 
 
-    <p class = "win">Player 1 Wins!</p>
-    <p class = "lose">Player 2 Loses!</p>
+        <button class = "start" type="button">New Game</button>
 
-    <button type="button">Start Game</button>
+
+    </div>
+
+
 
 
 
--- game.php
+++ game.php
@@ -13,116 +13,118 @@ require 'format.inc.php';
 </head>
 <body>
 
-<?php echo present_header(); ?>
+    <div class="content">
+        <div class = "header"><?php echo present_header(); ?></div>
 
-<form ...>
-    <div class="game">
-        <div class="row">
-            <div class="cell"><img src="images/valve-closed.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/ninety-sw.png" alt="cave" width="50" height="50" />
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell"><img src="images/gauge-top-0.png" alt="cave" width="50" height="50" /></div>
-        </div>
-        <div class="row">
-            <div class="cell">
-            </div><div class="cell"><img src="images/ninety-ne.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/gauge-0.png" alt="cave" width="50" height="50" /></div>
-        </div>
-        <div class="row">
-            <div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell">
-            </div><div class="cell">
-            </div><div class="cell"><img src="images/cap-s.png" alt="cave" width="50" height="50" />
-            </div><div class="cell">
-            </div><div class="cell"><img src="images/leak-s.png" alt="cave" width="50" height="50" />
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell"><img src="images/gauge-top-0.png" alt="cave" width="50" height="50" />
+        <form ...>
+            <div class="game">
+                <div class="row">
+                    <div class="cell"><img src="images/valve-closed.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/ninety-sw.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/gauge-top-0.png" alt="cave" width="50" height="50" /></div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell"><img src="images/ninety-ne.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/gauge-0.png" alt="cave" width="50" height="50" /></div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"></div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/cap-s.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/leak-s.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/gauge-top-0.png" alt="cave" width="50" height="50" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/tee-nes.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/tee-wne.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/leak-w.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell"><img src="images/gauge-0.png" alt="cave" width="50" height="50" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="cell"><img src="images/valve-closed.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell"><img src="images/ninety-wn.png" alt="cave" width="50" height="50" />
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div><div class="cell">
+                    </div>
+                </div>
+            </div>
+        </form>
+
+        <p class = "currentPlayer">Player 1, it is your turn!</p>
+
+        <div class="selection">
+            <div class="pipe">
+                <img src="images/ninety-es.png" alt="cave" width="50" height="50" />
+                <input type="radio" name="6" value="6x6" checked> <br>
+            </div>
+            <div class="pipe">
+                <img src="images/straight-h.png" alt="cave" width="50" height="50" />
+                <input type="radio" name="6" value="6x6" > <br>
+            </div>
+            <div class="pipe">
+                <img src="images/tee-esw.png" alt="cave" width="50" height="50" />
+                <input type="radio" name="6" value="6x6" > <br>
+            </div>
+            <div class="pipe">
+                <img src="images/cap-e.png" alt="cave" width="50" height="50" />
+                <input type="radio" name="6" value="6x6" > <br>
+            </div>
+            <div class="pipe">
+                <img src="images/tee-nes.png" alt="cave" width="50" height="50" />
+                <input type="radio" name="6" value="6x6" > <br>
             </div>
         </div>
-        <div class="row">
-            <div class="cell">
-            </div><div class="cell">
-            </div><div class="cell"><img src="images/tee-nes.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/tee-wne.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/leak-w.png" alt="cave" width="50" height="50" />
-            </div><div class="cell">
-            </div><div class="cell"><img src="images/gauge-0.png" alt="cave" width="50" height="50" />
+
+        <div class="choice">
+            <div class="button">
+                <button type="button">Rotate</button>
+            </div>
+            <div class="button">
+                <button type="button">Discard</button>
+            </div>
+            <div class="button">
+                <button type="button">Open Valve</button>
+            </div>
+            <div class="button">
+                <button type="button">Give Up</button>
             </div>
         </div>
-        <div class="row">
-            <div class="cell"><img src="images/valve-closed.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/straight-h.png" alt="cave" width="50" height="50" />
-            </div><div class="cell"><img src="images/ninety-wn.png" alt="cave" width="50" height="50" />
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div><div class="cell">
-            </div>
-        </div>
-    </div>
-</form>
 
-<p class = "currentPlayer">Player 1, it is your Turn!</p>
-
-<div class="selection">
-    <div class="pipe">
-        <img src="images/ninety-es.png" alt="cave" width="50" height="50" />
-        <input type="radio" name="6" value="6x6" checked> <br>
     </div>
-    <div class="pipe">
-        <img src="images/straight-h.png" alt="cave" width="50" height="50" />
-        <input type="radio" name="6" value="6x6" > <br>
-    </div>
-    <div class="pipe">
-        <img src="images/tee-esw.png" alt="cave" width="50" height="50" />
-        <input type="radio" name="6" value="6x6" > <br>
-    </div>
-    <div class="pipe">
-        <img src="images/cap-e.png" alt="cave" width="50" height="50" />
-        <input type="radio" name="6" value="6x6" > <br>
-    </div>
-    <div class="pipe">
-        <img src="images/tee-nes.png" alt="cave" width="50" height="50" />
-        <input type="radio" name="6" value="6x6" > <br>
-    </div>
-</div>
-
-<div class="choice">
-    <div class="button">
-        <button type="button">Rotate</button>
-    </div>
-    <div class="button">
-        <button type="button">Discard</button>
-    </div>
-    <div class="button">
-        <button type="button">Open Valve</button>
-    </div>
-    <div class="button">
-        <button type="button">Give Up</button>
-    </div>
-</div>
-
 
 </body>
 </html>
